0223ac5b82dc3820ed666785c73d2131560a6d84
Alex Rael <alprael22@gmail.com>
2019-01-08 13:50:47 -0700

    Boxed and sorted.

--- src/edu/cnm/deepdive/WeirdStream.java
+++ src/edu/cnm/deepdive/WeirdStream.java
@@ -8,8 +8,6 @@ import java.util.stream.IntStream;
 
 public class WeirdStream {
 
-  private Random rng;
-
   public static void main(String[] args) {
     IntStream.generate(new IntSupplier() {
       private Random rng;
@@ -23,21 +21,9 @@ public class WeirdStream {
         return rng.nextInt();
       }
     })
-        .limit(1000).forEach(System.out::println);
-
-  }
-
-  private static class RandomSupplier implements IntSupplier {
-
-    private Random rng;
-
-    {
-      rng = new Random(-1); // Reproducible sequence.
-    }
-
-    @Override
-    public int getAsInt() {
-      return rng.nextInt();
-    }
+        .limit(1000)
+        .boxed()
+        .sorted()
+        .forEach(System.out::println);
   }
 }
